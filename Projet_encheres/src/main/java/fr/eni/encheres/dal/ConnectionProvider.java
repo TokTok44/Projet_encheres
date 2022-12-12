9fc9e5dc2441324dfc4c27d8c7e328f6c0494830
TokTok44 <xavier.fort@outlook.fr>
2022-12-12 14:55:39 +0100

    commit de web.xml / context.xml / connectionprovider / testpoolconnection

---
+++ Projet_encheres/src/main/java/fr/eni/encheres/dal/ConnectionProvider.java
@@ -0,0 +1,32 @@
+package fr.eni.encheres.dal;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ConnectionProvider {
+
+	public static DataSource dataSource;
+	
+	//bloc exécuté 1 seule fois lors du 1er appel à la classe (soit en passant par une instance (new), soit méthode static, soit attribut static)
+	static {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+	
+}
+
+
+
